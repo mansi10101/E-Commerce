75ca25d1034fb19570f7ee24e2cc332d6fba119a
mansi10101 <mansinagaria01@gmail.com>
2023-10-06 11:59:23 +0530

    Server connected to mongodb

---
+++ server/index.js
@@ -0,0 +1,26 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+//express app
+const app = express();
+app.use(express.json()); //necessary for post and patch where we are requesting data
+app.use(bodyParser.json());
+
+// app.use((req, res, next) => {
+//   console.log(req.path, req.method);
+//   next();
+// });
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    //listen for requests
+    app.listen(process.env.PORT, () => {
+      console.log(`Connected to db & Listening at port ${process.env.PORT}...`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
